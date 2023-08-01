b73dac0ecfd1bdd10126a6025e9c7b0e12e5ab4d
Luizsellitto <luizsellitto2609@gmail.com>
2023-08-01 14:17:43 -0400

    Atividade v1

---
+++ Atividade Alunos List/Aluno.cs
@@ -0,0 +1,18 @@
+﻿public class Aluno
+{
+    public int Matricula { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public DateOnly DataNascimento { get; set; }
+    public Aluno()
+    {
+
+    }
+    public Aluno(int matricula, string nome, string cpf, DateOnly datanascimento)
+    {
+        this.Matricula = matricula;
+        this.Nome = nome;
+        this.CPF = cpf;
+        this.DataNascimento = datanascimento;
+    }
+}
---
+++ Atividade Alunos List/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Aluno> list = new List<Aluno>();
+        while (true)
+        {
+            Console.WriteLine("Digite 0 se for para para e 1 para continuar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            switch (numero)
+            {
+                case 0:
+                    foreach (Aluno a in list)
+                    {
+                        Console.WriteLine($"O nome do aluno é {a.Nome}, o CPF é {a.CPF}, a matrícola é {a.Matricula} e a Data de nascimento é {a.DataNascimento}");
+                    }
+                    goto exit;
+                case 1:
+                    Aluno aluno = new Aluno();
+                    Console.WriteLine("\n Digite o nome: ");
+                    aluno.Nome = Console.ReadLine();
+                    Console.WriteLine("\n Digite o CPF: ");
+                    aluno.CPF = Console.ReadLine();
+                    Console.WriteLine("\n Digite a matrícula: ");
+                    aluno.Matricula = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\n Digite a data em que este aluno nasceu: ");
+                    aluno.DataNascimento = DateOnly.Parse(Console.ReadLine());
+                    list.Add(aluno);
+                    break;
+                default:
+                    Console.WriteLine("Esta opção não exite\n");
+                    break;
+            }
+        }exit:;
+    }
+}
